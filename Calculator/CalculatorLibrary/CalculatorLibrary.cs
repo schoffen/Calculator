26e2c6e390f78e26eb815a8fc4b9b83eee0d0523
Felipe Schoffen <fs.tressino@gmail.com>
2025-11-05 06:19:15 -0300

    feat: change logfile from txt to json

--- Calculator/CalculatorLibrary/CalculatorLibrary.cs
+++ Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -2,49 +2,71 @@
 
 namespace CalculatorLibrary;
 using System.Diagnostics;
+using Newtonsoft.Json;
 
 public class Calculator
 {
+    JsonWriter jsonWriter;
+    
     public Calculator()
     {
-        StreamWriter logFile = File.CreateText("calculator.log");
-        Trace.Listeners.Add(new TextWriterTraceListener(logFile));
-        Trace.AutoFlush = true;
-        Trace.WriteLine("Starting Calculator log");
-        Trace.WriteLine($"Started {DateTime.Now.ToString(CultureInfo.CurrentCulture)}");
+        StreamWriter logFile = File.CreateText("calculatorlog.json");
+        Console.WriteLine(Path.GetFullPath("calculatorLog.json"));
+        logFile.AutoFlush = true;
+        jsonWriter = new JsonTextWriter(logFile);
+        jsonWriter.Formatting = Formatting.Indented;
+        jsonWriter.WriteStartObject();
+        jsonWriter.WritePropertyName("Operations");
+        jsonWriter.WriteStartArray();
     }
     
     public double DoOperation(double num1, double num2, string op)
     {
         double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
+        jsonWriter.WriteStartObject();
+        jsonWriter.WritePropertyName("Operand1");
+        jsonWriter.WriteValue(num1);
+        jsonWriter.WritePropertyName("Operand2");
+        jsonWriter.WriteValue(num2);
+        jsonWriter.WritePropertyName("Operation");
         // Use a switch statement to do the math.
         switch (op)
         {
             case "a":
                 result = num1 + num2;
-                Trace.WriteLine(String.Format($"{num1} + {num2} = {result}"));
+                jsonWriter.WriteValue("Add");
                 break;
             case "s":
                 result = num1 - num2;
-                Trace.WriteLine(String.Format($"{num1} - {num2} = {result}"));
+                jsonWriter.WriteValue("Subtract");
                 break;
             case "m":
                 result = num1 * num2;
-                Trace.WriteLine(String.Format($"{num1} * {num2} = {result}"));
+                jsonWriter.WriteValue("Multiply");
                 break;
             case "d":
                 // Ask the user to enter a non-zero divisor.
                 if (num2 != 0)
                 {
                     result = num1 / num2;
-                    Trace.WriteLine(String.Format($"{num1} / {num2} = {result}"));
                 }
+                jsonWriter.WriteValue("Divide");
                 break;
             // Return text for an incorrect option entry.
             default:
                 break;
         }
+        jsonWriter.WritePropertyName("Result");
+        jsonWriter.WriteValue(result);
+        jsonWriter.WriteEndObject();
+
         return result;
+    }
+
+    public void Finish()
+    {
+        jsonWriter.WriteEndArray();
+        jsonWriter.WriteEndObject();
+        jsonWriter.Close();
     }
 }
